e6606738aa5a3558c324975e4f5f14a2f2e8d48e
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-07-26 10:59:42 -0300

    FEAT(gitInit): Adiciona comando git init, criando os diretórios estruturais do git.

---
+++ Csharp/Commands/Init.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Commands
+{
+    public class GitInit
+    {
+        public static void Execute()
+        {
+            string gitDir = Path.Combine(Directory.GetCurrentDirectory(), ".gitadr");
+
+            if (Directory.Exists(gitDir))
+            {
+                Console.WriteLine("Repositório já existe.");
+                return;
+            }
+
+            Directory.CreateDirectory(gitDir);
+            Directory.CreateDirectory(Path.Combine(gitDir, "objects"));
+            Directory.CreateDirectory(Path.Combine(gitDir, "refs"));
+
+            File.WriteAllText(Path.Combine(gitDir, "HEAD"), "ref: refs/heads/master\n");
+
+            Console.WriteLine($"Repositório inicializado em {gitDir}");
+        }
+    }
+}
--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Csharp.Commands;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Uso: dotnet run -- <comando_git>");
+    return;
+}
+
+
+switch (args[0])
+{
+    case "init":
+        GitInit.Execute();
+        break;
+
+    default:
+        Console.WriteLine($"Comando desconhecido: {args[0]}");
+        break;
+}
