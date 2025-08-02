81ba0a9ca68845d94e098e9e06deb525fb3da767
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-02 20:18:06 -0300

    FEAT(write-tree): Gera a tree completa com os dois modes: 100644 e 040000
    - Gera objetos blobs (arquivos, mode = 100644)
    - Gera objetos trees (diretorios, mode = 040000)

--- Git/Commands/WriteTree.cs
+++ Git/Commands/WriteTree.cs
@@ -1,87 +1,56 @@
 ﻿using Csharp.Core;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Csharp.Commands
 {
     public class WriteTree
     {
-
-        private static string[] ignoreFiles = { ".gitadr", "Program.cs", "Git.csproj", "bin", "Commands", "Core", "obj", ".vs" };
+        private static readonly string[] ignoreFiles = { ".gitadr", "Program.cs", "Git.csproj", "bin", "Commands", "Core", "obj", ".vs" };
 
         public static void Execute()
         {
+            var rootTreeSha1 = WriteTreeRecursive(Directory.GetCurrentDirectory());
+            Console.WriteLine(rootTreeSha1);
+        }
+
+        private static string WriteTreeRecursive(string directory)
+        {
             var entries = new List<TreeEntry>();
 
-            ExecuteDirectory(new List<string>() { Directory.GetCurrentDirectory() });
-
-            //CreateBlobObject(files, entries);
-
-            ////foreach (var file in files)
-            ////{
-            ////    entries.Add(new TreeEntry
-            ////    {
-            ////        Mode = "100644",
-            ////        Name = Path.GetFileName(file),
-            ////        Sha1 = blobSha1
-            ////    });
-            ////}
-            
-            //string treeSha1 = TreeObject.WriteTree(entries);
-            //Console.WriteLine(treeSha1);
-        }
-
-        public static List<string> GetAllFiles(string currentDirectory)
-            => Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), currentDirectory)).Where(f => !ignoreFiles.Where(i => f.Contains(i)).Any()).ToList();
-
-        public static List<string> GetAllDirectories(string currentDirectory)
-            => Directory.GetDirectories(Path.Combine(Directory.GetCurrentDirectory(), currentDirectory)).Where(d => !ignoreFiles.Where(i => d.Contains(i)).Any()).ToList();
-
-        public static void CreateBlobObject(List<string> files)
-        {
-            foreach (var file in files)
+            foreach (var file in Directory.GetFiles(directory))
             {
-                if (ignoreFiles.Any(ignore => file.Contains(ignore)))
+                if (ignoreFiles.Any(ignore => file.Contains(ignore))) continue;
+
+                var (blobSha1, fullBlob) = Utils.WriteBlob(file);
+                ObjectStore.WriteObject(blobSha1, fullBlob);
+
+                entries.Add(new TreeEntry
+                {
+                    Mode = "100644",
+                    Name = Path.GetFileName(file),
+                    Sha1 = blobSha1
+                });
+            }
+
+            foreach (var subdir in Directory.GetDirectories(directory))
+            {
+                if (ignoreFiles.Any(ignore => subdir.Contains(ignore)))
                 {
                     continue;
                 }
 
-                var (blobSha1, fullBlob) = Utils.WriteBlob(file);
-                ObjectStore.WriteObject(blobSha1, fullBlob);
-            }
-        }
+                var treeSha1 = WriteTreeRecursive(subdir);
 
-        public static void CreateTreeObject(List<string> files)
-        {
-            foreach (var file in files)
-            {
-                if (ignoreFiles.Any(ignore => file.Contains(ignore)))
+                entries.Add(new TreeEntry
                 {
-                    continue;
-                }
-
-                var (blobSha1, fullBlob) = Utils.WriteBlob(file);
-                ObjectStore.WriteObject(blobSha1, fullBlob);
+                    Mode = "040000",
+                    Name = Path.GetFileName(subdir),
+                    Sha1 = treeSha1
+                });
             }
-        }
 
-        public static void ExecuteDirectory(List<string> currentDirectories)
-        {
-            if (currentDirectories.Count > 0) 
-            {
-                Console.WriteLine(currentDirectories[0]);
-                var currentDirectory = currentDirectories[0];
-                var files = GetAllFiles(currentDirectory);
-
-                CreateBlobObject(files);
-
-                currentDirectories = currentDirectories.Where(c => c != currentDirectory).ToList();
-
-                currentDirectories.AddRange(GetAllDirectories(currentDirectory));
-
-                Console.WriteLine(currentDirectories.Count);
-
-                ExecuteDirectory(currentDirectories);
-            }
+            return TreeObject.WriteTree(entries);
         }
     }
 }
