7025dc9e0c3703cee13f4f1b2109584a58ef5fa0
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-03 22:52:24 -0300

    CHORE(ObjectStore): Adiciona metodo ReadObject().
    - Le objeto e realiza validações
    - Retorna objeto lido em bytes

--- Git/Commands/CatFile.cs
+++ Git/Commands/CatFile.cs
@@ -1,4 +1,4 @@
-﻿using System.IO.Compression;
+﻿using Csharp.Core;
 using System.Text;
 
 namespace Csharp.Commands
@@ -13,35 +13,11 @@ namespace Csharp.Commands
                 return string.Empty;
             }
 
-            var hash = args[1];
-
-            var dir = Path.Combine(".gitadr", "objects", hash.Substring(0, 2));
-            var file = hash.Substring(2);
-            var path = Path.Combine(dir, file);
-
-            if (!Directory.Exists(dir) || !File.Exists(path))
-            {
-                Console.WriteLine($"Objeto não encontrado: ${hash} não encontrado.");
-                return string.Empty;
-            }
-
-            using var fs = File.OpenRead(path);
-            using var zlib = new ZLibStream(fs, CompressionMode.Decompress);
-            using var outputStream = new MemoryStream();
-
-            zlib.CopyTo(outputStream);
-            var data = outputStream.ToArray();
-
+            var data = ObjectStore.ReadObject(args[1]);
             var nullIndex = Array.IndexOf(data, (byte)0);
-            if (nullIndex == -1)
-            {
-                Console.WriteLine("Objeto inválido (sem header)");
-                return string.Empty;
-            }
-
             var content = Encoding.UTF8.GetString(data[(nullIndex + 1)..]);
-            Console.WriteLine(content);
 
+            Console.WriteLine(content);
             return content;
         }
     }
