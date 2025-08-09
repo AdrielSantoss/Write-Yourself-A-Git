52764f1abd756f9806b591fa6d6c105e73aed905
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-09 13:11:55 -0300

    FEAT(utils): Adiciona Sha1BytesToString() e GetObjectDataBySha1().

--- Git.Core/Utils.cs
+++ Git.Core/Utils.cs
@@ -1,11 +1,11 @@
-﻿using System.Security.Cryptography;
+﻿using System.IO.Compression;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Csharp.Core
 {
     public class Utils
     {
-
         public static byte[] CombineBytes(byte[] a, byte[] b)
         {
             var combined = new byte[a.Length + b.Length];
@@ -52,6 +52,38 @@ namespace Csharp.Core
                 bytes[i] = Convert.ToByte(byteString, 16);
             }
             return bytes;
+        }
+
+        public static string Sha1BytesToString(byte[] sha1)
+        {
+            return BitConverter.ToString(sha1).Replace("-", "").ToLower();
+        }
+
+        public static byte[] GetObjectDataBySha1(string hash)
+        {
+            var dir = Path.Combine(".gitadr", "objects", hash.Substring(0, 2));
+            var file = hash.Substring(2);
+            var path = Path.Combine(dir, file);
+
+            if (!Directory.Exists(dir) || !File.Exists(path))
+            {
+                throw new Exception($"Objeto não encontrado: ${hash} não encontrado.");
+            }
+
+            using var fs = File.OpenRead(path);
+            using var zlib = new ZLibStream(fs, CompressionMode.Decompress);
+            using var outputStream = new MemoryStream();
+
+            zlib.CopyTo(outputStream);
+            var data = outputStream.ToArray();
+
+            var nullIndex = Array.IndexOf(data, (byte)0);
+            if (nullIndex == -1)
+            {
+                throw new Exception("Objeto inválido (sem header)");
+            }
+
+            return data;
         }
     }
 }
