05b2c6b7bb9c245ae0f4e2ec07551de5a9290302
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-09 13:13:25 -0300

    CHORE(objectStore): Remove ReadObject() inutilizado.
    - ReadObject() foi substituido por Utils.GetObjectDataBySha1()

--- Git/Core/ObjectStore.cs
+++ Git/Core/ObjectStore.cs
@@ -21,32 +21,5 @@ namespace Csharp.Core
             using var zlib = new ZLibStream(fs, CompressionMode.Compress);
             zlib.Write(data, 0, data.Length);
         }
-
-        public static byte[] ReadObject(string hash)
-        {
-            var dir = Path.Combine(".gitadr", "objects", hash.Substring(0, 2));
-            var file = hash.Substring(2);
-            var path = Path.Combine(dir, file);
-
-            if (!Directory.Exists(dir) || !File.Exists(path))
-            {
-                throw new Exception($"Objeto não encontrado: ${hash} não encontrado.");
-            }
-
-            using var fs = File.OpenRead(path);
-            using var zlib = new ZLibStream(fs, CompressionMode.Decompress);
-            using var outputStream = new MemoryStream();
-
-            zlib.CopyTo(outputStream);
-            var data = outputStream.ToArray();
-
-            var nullIndex = Array.IndexOf(data, (byte)0);
-            if (nullIndex == -1)
-            {
-                throw new Exception("Objeto inválido (sem header)");
-            }
-
-            return data;
-        }
     }
 }
