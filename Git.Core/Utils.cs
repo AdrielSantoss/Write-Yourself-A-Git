0ec9229632242b935693c7b070a3ea6d2b222502
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-09 14:55:37 -0300

    CHORE(Utils): Adiciona chamada de Sha1BytesToString().
    - Substitui expressão repetida por Sha1BytesToString().

--- Git.Core/Utils.cs
+++ Git.Core/Utils.cs
@@ -6,41 +6,9 @@ namespace Csharp.Core
 {
     public class Utils
     {
-        public static byte[] CombineBytes(byte[] a, byte[] b)
+        public static string Sha1BytesToString(byte[] sha1)
         {
-            var combined = new byte[a.Length + b.Length];
-            Buffer.BlockCopy(a, 0, combined, 0, a.Length);
-            Buffer.BlockCopy(b, 0, combined, a.Length, b.Length);
-            return combined;
-        }
-
-        public static string ComputeSha1(byte[] data)
-        {
-            using var sha1 = SHA1.Create();
-            var hashBytes = sha1.ComputeHash(data);
-            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-        }
-
-        public static string GetSha1FromBlob(string path)
-        {
-            byte[] content = File.ReadAllBytes(path);
-            byte[] header = Encoding.UTF8.GetBytes($"blob {content.Length}\0");
-            byte[] fullBlob = CombineBytes(header, content);
-
-            using var sha1 = SHA1.Create();
-            byte[] hashBytes = sha1.ComputeHash(fullBlob);
-
-            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-        }
-
-        public static (string sha1, byte[] fullBlob) WriteBlob(string filePath)
-        {
-            var content = File.ReadAllBytes(filePath);
-            var header = $"blob {content.Length}\0";
-            var fullBlob = CombineBytes(Encoding.UTF8.GetBytes(header), content);
-            var sha1 = ComputeSha1(fullBlob);
-
-            return (sha1, fullBlob);
+            return BitConverter.ToString(sha1).Replace("-", "").ToLower();
         }
 
         public static byte[] StringToSha1Bytes(string sha1)
@@ -54,9 +22,54 @@ namespace Csharp.Core
             return bytes;
         }
 
-        public static string Sha1BytesToString(byte[] sha1)
+        public static byte[] CombineBytes(byte[] a, byte[] b)
         {
-            return BitConverter.ToString(sha1).Replace("-", "").ToLower();
+            var combined = new byte[a.Length + b.Length];
+            Buffer.BlockCopy(a, 0, combined, 0, a.Length);
+            Buffer.BlockCopy(b, 0, combined, a.Length, b.Length);
+            return combined;
+        }
+
+        public static string ComputeSha1(byte[] data)
+        {
+            using var sha1 = SHA1.Create();
+            var hashBytes = sha1.ComputeHash(data);
+
+            return Sha1BytesToString(hashBytes);
+        }
+
+        public static string GetSha1FromBlob(string path)
+        {
+            byte[] content = File.ReadAllBytes(path);
+            byte[] header = Encoding.UTF8.GetBytes($"blob {content.Length}\0");
+            byte[] fullBlob = CombineBytes(header, content);
+
+            using var sha1 = SHA1.Create();
+            byte[] hashBytes = sha1.ComputeHash(fullBlob);
+
+            return Sha1BytesToString(hashBytes);
+        }
+
+        public static string GetSha1FromTree(string path)
+        {
+            byte[] content = File.ReadAllBytes(path);
+            byte[] header = Encoding.UTF8.GetBytes($"blob {content.Length}\0");
+            byte[] fullBlob = CombineBytes(header, content);
+
+            using var sha1 = SHA1.Create();
+            byte[] hashBytes = sha1.ComputeHash(fullBlob);
+
+            return Sha1BytesToString(hashBytes);
+        }
+
+        public static (string sha1, byte[] fullBlob) WriteBlob(string filePath)
+        {
+            var content = File.ReadAllBytes(filePath);
+            var header = $"blob {content.Length}\0";
+            var fullBlob = CombineBytes(Encoding.UTF8.GetBytes(header), content);
+            var sha1 = ComputeSha1(fullBlob);
+
+            return (sha1, fullBlob);
         }
 
         public static byte[] GetObjectDataBySha1(string hash)
