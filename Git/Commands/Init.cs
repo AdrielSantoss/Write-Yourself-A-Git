427a996503b8ce793f8e4b05657d6a16c8f31c1a
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-10 13:34:56 -0300

    FEAT(init): Adiciona criação de refs/heads/master.

--- Git/Commands/Init.cs
+++ Git/Commands/Init.cs
@@ -8,14 +8,16 @@
 
             if (Directory.Exists(gitDir))
             {
-                Console.WriteLine("Repositório já existe.");
+                Console.WriteLine("Repositório .gitadr já existe.");
                 return;
             }
 
             Directory.CreateDirectory(gitDir);
             Directory.CreateDirectory(Path.Combine(gitDir, "objects"));
             Directory.CreateDirectory(Path.Combine(gitDir, "refs"));
+            Directory.CreateDirectory(Path.Combine(gitDir, "refs", "heads"));
 
+            File.Create(Path.Combine(gitDir, "refs", "heads", "master"));
             File.WriteAllText(Path.Combine(gitDir, "HEAD"), "ref: refs/heads/master\n");
 
             Console.WriteLine($"Repositório inicializado em {gitDir}");
