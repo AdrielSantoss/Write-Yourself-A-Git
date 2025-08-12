e4c1805075359e1b6492c58949acb35698d24782
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-12 20:40:47 -0300

    FEAT(addTest): Adiciona teste unitário do comando add.

---
+++ Git.Test/AddTest.cs
@@ -0,0 +1,34 @@
+﻿using Csharp.Core;
+using Csharp.Test.Configs;
+using Git.Commands;
+using System.Text;
+namespace Git.Test
+{
+    public class AddTest : IClassFixture<InitFixture>
+    {
+        [Fact]
+        public void Add_HashBlobAndWriteIndex()
+        {
+            var fileName = "testeAdd.txt";
+            var content = "test add";
+            File.WriteAllText(fileName, content, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+
+            var sha1 = Utils.GetSha1FromBlob(fileName);
+
+            Add.Execute([fileName]);
+
+            var lines = Utils.GetIndexFileContentLines();
+            var parts = new List<string>();
+
+            foreach (var line in lines)
+            {
+                parts.AddRange(line.Split(" ", 2));
+            }
+
+            Assert.NotEmpty(lines);
+            Assert.NotEmpty(parts);
+            Assert.Contains(fileName, parts);
+            Assert.Contains(sha1, parts);
+        }
+    }
+}
