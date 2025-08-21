6b45d3672e50680614d514e8c268eb9ca0d1bbb6
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-08-20 21:28:30 -0300

    FIX(switch): Corrige validação ao alterar de branch.
    - Não estava levando em consideração o que foi "commitado" no novo branch.

--- Git/Commands/Switch.cs
+++ Git/Commands/Switch.cs
@@ -29,13 +29,18 @@ namespace Git.Commands
             var targetBranchTreeSha1 = CommitUtils.GetCommitTreeSha1(targetBranchHead!);
             var targetBranchEntries = TreeUtils.GetTreeEntriesFromSha1("", targetBranchTreeSha1, new Dictionary<string, (string Mode, string Sha1)>());
 
-            var fullEntries = workSpaceEntries.Keys.Union(indexEntries.Keys);
+            var commitHead = CommitUtils.GetLastCommitSha1FromHead();
+            var commitHeadTreeSha1 = CommitUtils.GetCommitTreeSha1(commitHead);
+            var headEntries = TreeUtils.GetTreeEntriesFromSha1("", commitHeadTreeSha1, new Dictionary<string, (string Mode, string Sha1)>());
 
+            var fullEntries = workSpaceEntries.Keys.Union(indexEntries.Keys);
+            
             foreach (var entry in fullEntries)
             {
                 var wsSha1 = workSpaceEntries.ContainsKey(entry) ? workSpaceEntries[entry] : null;
                 var idxSha1 = indexEntries.ContainsKey(entry) ? indexEntries[entry] : null;
                 var tgtSha1 = targetBranchEntries.ContainsKey(entry) ? targetBranchEntries[entry].Sha1 : null;
+                var headSha1 = headEntries.FirstOrDefault(e => e.Key == entry).Value.Sha1;
 
                 if (idxSha1 == null && wsSha1 != null && tgtSha1 != null)
                 {
@@ -49,14 +54,12 @@ namespace Git.Commands
                     return;
                 }
 
-                if (tgtSha1 != null && idxSha1 != tgtSha1)
+                if (tgtSha1 != null && idxSha1 != null && idxSha1 != tgtSha1 && idxSha1 != headSha1) // continuar aqui, testar
                 {
                     Console.WriteLine($"Existem mudanças staged que conflitam com o branch alvo para '{entry}'.");
                     return;
                 }
-            } 
-
-            BranchUtils.WriteHead(@$"ref: refs\heads\{branchName}");
+            }
 
             var indexLines = new List<string>();
                
@@ -91,6 +94,8 @@ namespace Git.Commands
                     continue;
                 }
             }
+
+            BranchUtils.WriteHead(@$"ref: refs\heads\{branchName}");
 
             Console.WriteLine($"Branch atual alterado com sucesso para {branchName}");
         }
