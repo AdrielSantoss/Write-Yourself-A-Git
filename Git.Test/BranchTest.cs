f6ab6c735eb3ada7ac5a916313db4f010d17104c
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-08 18:35:29 -0300

    FEAT(branchTest): Adiciona teste unitário do comando "gitadr branch"
    - Testa criação do arquivo de branch.
    - Valida ponteiro do branch

---
+++ Git.Test/BranchTest.cs
@@ -0,0 +1,41 @@
+﻿using Csharp.Test.Configs;
+using Git.Commands;
+using Git.Core;
+using System.Text;
+namespace Git.Test
+{
+    public class BranchTest : IClassFixture<InitFixture>
+    {
+        [Theory]
+        [InlineData("branchTest")]
+        public void Branch_CreateBranchFile(string branchName)
+        {
+            var fileName = "testeCommit.txt";
+            var content = "test commit";
+            File.WriteAllText(fileName, content, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+
+            Add.Execute([fileName]);
+
+            var indexEntries = IndexUtils.GetIndexEntries();
+            Assert.NotEmpty(indexEntries);
+
+            var indexEntry = indexEntries[fileName];
+            Assert.NotNull(indexEntry);
+
+            var sha1BlobExpected = BlobUtils.GetSha1FromBlob(fileName);
+            Assert.Contains(sha1BlobExpected, indexEntries.Values);
+
+            var commitSha1 = Commit.Execute(["-m", "commit test"]);
+            Assert.NotNull(commitSha1);
+
+            Branch.Execute([branchName]);
+
+            var gitDir = Path.Combine(Directory.GetCurrentDirectory(), ".gitadr");
+            var branchPath = Path.Combine(gitDir, $"refs{Path.DirectorySeparatorChar}heads{Path.DirectorySeparatorChar}{branchName}");
+            Assert.True(Path.Exists(branchPath));
+
+            var branchContent = File.ReadAllText(branchPath);
+            Assert.Equal(commitSha1, branchContent);
+        }
+    }
+}
