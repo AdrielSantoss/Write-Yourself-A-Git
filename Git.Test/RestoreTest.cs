2c4db3fe36e1fd9f5bcfcbc936a461d21ca2d24e
Adriel dos Santos <adrieldossantos7@gmail.com>
2025-09-08 20:01:26 -0300

    FEAT(restoreTest): Adiciona teste unitário do comando "gitadr restore".
    - Testa arquivo modificado que não esta na staging area.

---
+++ Git.Test/RestoreTest.cs
@@ -0,0 +1,39 @@
+﻿using Csharp.Test.Configs;
+using Git.Commands;
+using Git.Core;
+using System.Text;
+namespace Git.Test
+{
+    public class RestoreTest : IClassFixture<InitFixture>
+    {
+        [Theory]
+        [InlineData("restoreTest", "restoreTest 1")]
+        public void Restore_RestoreChanges(string fileName, string fileContent)
+        {
+            File.WriteAllText(fileName, fileContent, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+
+            Add.Execute([fileName]);
+
+            var indexEntries = IndexUtils.GetIndexEntries();
+            Assert.NotEmpty(indexEntries);
+
+            var indexEntry = indexEntries[fileName];
+            Assert.NotNull(indexEntry);
+
+            var sha1BlobExpected = BlobUtils.GetSha1FromBlob(fileName);
+            Assert.Contains(sha1BlobExpected, indexEntries.Values);
+
+            var commitSha1 = Commit.Execute(["-m", "commit restore test"]);
+            Assert.NotNull(commitSha1);
+
+            File.WriteAllText(fileName, "new content", new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+            var newSha1BlobFile = BlobUtils.GetSha1FromBlob(fileName);
+            Assert.NotEqual(newSha1BlobFile, sha1BlobExpected);
+
+            Restore.Execute([fileName]);            
+
+            newSha1BlobFile = BlobUtils.GetSha1FromBlob(fileName);
+            Assert.Equal(newSha1BlobFile, sha1BlobExpected);
+        }
+    }
+}
